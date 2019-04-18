5272f6d6b5ff2a0c5e172f66fd85fa9af08117ff
bboy sj <985060683@qq.com>
2019-04-18 14:55:55 +0800

    Initial commit

---
+++ sjlianxi/src/main/java/com/baizhi/sjlianxi/SjlianxiApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.sjlianxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjlianxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjlianxiApplication.class, args);
+    }
+
+}
